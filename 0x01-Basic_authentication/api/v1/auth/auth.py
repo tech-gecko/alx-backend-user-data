3dd44bbda66d654308c85d3a03ecfa00521d5602
tech-gecko <w0nderzz2001@gmail.com>
2024-11-12 23:54:09 +0100

    Add documentation for methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,19 @@ class Auth:
         Auth class containing auth functions.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+            Checks if a given path requires auth.
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+            Gets the auth header for a given request (if not None).
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+            Gets the current user from the request.
+        """
         return None
