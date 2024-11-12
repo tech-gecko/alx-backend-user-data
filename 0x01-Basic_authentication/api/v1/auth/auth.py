23385e294dcf47d57b82c12ffbc2bbdda92b44a0
tech-gecko <w0nderzz2001@gmail.com>
2024-11-13 00:18:59 +0100

    Optimize for pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,16 +14,16 @@ class Auth:
         """
             Checks if a given path requires auth.
         """
-        if path is None or excluded_paths is None: 
+        if path is None or excluded_paths is None:
             return True
         if excluded_paths == []:
             return True
-        
+
         if path[-1] != '/':
             path = path + '/'
         if path in excluded_paths:
             return False
-        
+
         return True
 
     def authorization_header(self, request=None) -> str:
