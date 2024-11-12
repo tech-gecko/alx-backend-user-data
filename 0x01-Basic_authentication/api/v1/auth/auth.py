5bf7b98190a1f6ee04f81249ad501a0aaae75619
tech-gecko <w0nderzz2001@gmail.com>
2024-11-12 23:40:53 +0100

    Check pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,11 +12,9 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
-    
 
     def authorization_header(self, request=None) -> str:
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         return None
