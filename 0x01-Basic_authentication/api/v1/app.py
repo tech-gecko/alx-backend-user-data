7377176b61dcc74badb98aa0c547e97fda209d07
tech-gecko <w0nderzz2001@gmail.com>
2024-11-12 21:46:15 +0100

    Update 'api/v1/views/index.py' and 'api/v1/views/index.py' to support error 401

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,16 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    response = {
+        "error": "Unauthorized"
+    }
+    return jsonify(response), 401
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
