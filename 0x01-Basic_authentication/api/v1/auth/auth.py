0ee11b0e6f1c8bdf4b2b77b3ce4d7fcd12cbd8f0
tech-gecko <w0nderzz2001@gmail.com>
2024-11-13 00:16:27 +0100

    Create slash tolerant check

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,17 @@ class Auth:
         """
             Checks if a given path requires auth.
         """
-        return False
+        if path is None or excluded_paths is None: 
+            return True
+        if excluded_paths == []:
+            return True
+        
+        if path[-1] != '/':
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+        
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
