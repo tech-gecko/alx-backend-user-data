c232c279347a3b831210c16984d581cb905b3fd4
tech-gecko <w0nderzz2001@gmail.com>
2024-11-13 15:20:38 +0100

    Add method to decode base64-encoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
     Module containing the 'BasicAuth' class.
 """
 from .auth import Auth
-from flask import request
+import base64
+import binascii
 import re
 
 
@@ -14,7 +15,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """
-            Method that extracts the base64 encoded header from the
+            Extracts the base64-encoded header from the
             header of the request using Basic Authentication.
         """
         if (
@@ -25,3 +26,25 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+            Decodes the base64-encoded string gotten from the method above.
+        """
+        if (
+            base64_authorization_header is None or
+            not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        try:
+            decoded_bytes = base64.decode(
+                base64_authorization_header,
+                validate=True
+            )
+            decoded_str = decoded_bytes.decode('utf-8')
+
+            return decoded_str
+        except binascii.Error:
+            return None
