fdabd57839fd75636023671ffe0b5bf59a0e4061
tech-gecko <w0nderzz2001@gmail.com>
2024-11-13 14:53:17 +0100

    Add documentation for the extraction method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,6 +13,10 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
+        """
+            Method that extracts the base64 encoded header from the
+            header of the request using Basic Authentication.
+        """
         if (
             authorization_header is None or
             not isinstance(authorization_header, str) or
