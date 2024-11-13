73da4998fb40ce4493b3d429c8d1b755c50c8c61
tech-gecko <w0nderzz2001@gmail.com>
2024-11-13 22:02:11 +0100

    Optimize to return None if exception (no user with provided mail)

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -81,7 +81,10 @@ class BasicAuth(Auth):
         ):
             return None
 
-        users = User.search({'email': user_email})
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
         if not users:
             return None
         user = users[0]
