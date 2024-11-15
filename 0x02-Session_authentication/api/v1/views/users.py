1b7c722023d6b0c4cd6835ebe97d1c5836bb8383
tech-gecko <w0nderzz2001@gmail.com>
2024-11-15 14:20:09 +0100

    Add support for when user ID is 'me'and user is validated

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,12 +27,19 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    if user_id == 'me' and request.current_user is None:
-        abort(404)
 
     user = User.get(user_id)
     if user is None:
         abort(404)
+
+    """
+        Session auth for GET '/api/v1/users/me'.
+    """
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        else:
+            return jsonify(user.to_json())
 
     return jsonify(user.to_json())
 
