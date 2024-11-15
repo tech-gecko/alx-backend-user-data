cb6989aac986188bbdbae63cc468e28142070e79
tech-gecko <w0nderzz2001@gmail.com>
2024-11-15 21:45:05 +0100

    Add template for session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,9 @@ if auth_type == 'auth':
 elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(401)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Module containing the 'SessionAuth' class.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+        SessionAuth class containing functions for Session authentication.
+    """
+    pass
