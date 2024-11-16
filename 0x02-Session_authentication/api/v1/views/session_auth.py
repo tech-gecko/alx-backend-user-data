3b077d96ca91f11e40e649c81ea1bd52a6923f20
tech-gecko <w0nderzz2001@gmail.com>
2024-11-17 00:56:54 +0100

    Add type-annotation

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -15,33 +15,32 @@ def create_session() -> Tuple[str, int]:
         Return:
             Session for authenticated user.
     """
+    not_found_resp = {"error": "no user found for this email"}
+
     email = request.form.get('email')
-    if not email:
-        return jsonify({'error': 'email missing'}), 400
+    if email is None or len(email.strip()) == 0:
+        return jsonify({"error": "email missing"}), 400
 
     password = request.form.get('password')
-    if not password:
-        return jsonify({'error': 'password missing'}), 400
+    if password is None or len(password.strip()) == 0:
+        return jsonify({"error": "password missing"}), 400
 
     try:
         users = User.search({'email': email})
     except Exception:
-        return jsonify({'error': "no user found for this email"}), 404
+        return jsonify(not_found_resp), 404
     if len(users) <= 0:
-        return jsonify({'error': "no user found for this email"}), 404
+        return jsonify(not_found_resp), 404
 
-    user = users[0]
-    if user.is_valid_password(password):
-        response = jsonify(user.to_json())
-
+    if users[0].is_valid_password(password):
         from api.v1.app import auth
-        session_id = auth.create_session(user.id)
-        cookie_name = getenv('SESSION_NAME')
-        response.set_cookie(cookie_name, session_id)
+        sessiond_id = auth.create_session(getattr(users[0], 'id'))
+        resp = jsonify(users[0].to_json())
+        resp.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
 
-        return response, 201
+        return resp
 
-    return jsonify({'error': 'wrong password'}), 401
+    return jsonify({"error": "wrong password"}), 401
 
 
 @app_views.route('/auth_session/logout', methods=['DELETE'],
