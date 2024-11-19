70d4ae06086b6aa978e0f0b413052cf612e47dd3
tech-gecko <w0nderzz2001@gmail.com>
2024-11-19 16:32:54 +0100

    Remove '_hash_password()' from class Auth in auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,22 +8,26 @@ from sqlalchemy.orm.exc import NoResultFound
 from user import User
 
 
+def _hash_password(self, password: str) -> bytes:
+    """
+        Takes in a password string argument and returns bytes.
+        The returned bytes is a salted hash of the input password.
+    """
+    if password is None:
+        return None
+
+    # Convert str to bytes for use in bcrypt.hashpw
+    bytes_password = password.encode("utf-8")
+    hashed_password = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
+
+    return hashed_password
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """
-            Takes in a password string argument and returns bytes.
-            The returned bytes is a salted hash of the input password.
-        """
-        # Convert str to bytes for use in bcrypt.hashpw
-        bytes_password = password.encode("utf-8")
-        hashed_password = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
-
-        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """
@@ -33,7 +37,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
         else:
