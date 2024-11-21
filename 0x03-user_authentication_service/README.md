bb29d81170799bff7f5f4dc43bf04cbc9e91c89b
tech-gecko <w0nderzz2001@gmail.com>
2024-11-21 21:13:14 +0100

    Add login() method to app.py to log in user if info is valid

--- 0x03-user_authentication_service/README.md
+++ 0x03-user_authentication_service/README.md
@@ -1,2 +1,3 @@
 # User Authentication Service
 This directory contains tasks on user authentication for an application. It uses a Flask app and SQLAlchemy ORM for the DB.
+app.py interacts with auth.py, which in turn interacts with db.py, which in turn interacts with user.py
--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
     A simple Flask app with user authentication services.
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -33,6 +33,24 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """
+        Logs the user in if login info is correct.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    new_session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', new_session_id)
+
+    return response
 
 
 if __name__ == "__main__":
