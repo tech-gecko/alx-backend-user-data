7816d11d2bed839d4b62f6de3c681b1b09ef5ec8
tech-gecko <w0nderzz2001@gmail.com>
2024-11-21 14:00:58 +0100

    Add type-annotation for index method in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index():
+def index() -> str:
     """
         GET /
         Return:
