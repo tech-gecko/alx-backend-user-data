c7864158dcf6d1f34b73107a8251a96676450665
tech-gecko <w0nderzz2001@gmail.com>
2024-11-21 14:46:55 +0100

    Add route for registering a new user in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
     A simple Flask app with user authentication services.
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -15,6 +17,22 @@ def index() -> str:
          - The home page view.
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+        POST /users
+        Registers a user if the user doesn't already exist in the DB.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
