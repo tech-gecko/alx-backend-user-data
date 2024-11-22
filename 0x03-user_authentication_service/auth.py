c1e95886f2887d48b0574fcee9d1377237eb76fe
tech-gecko <w0nderzz2001@gmail.com>
2024-11-22 12:31:21 +0100

    Raise ValueError if there's an exception instead of returning None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -121,8 +121,6 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except Exception:
-            return None
-        if not user:
             raise ValueError
 
         reset_token = _generate_uuid()
